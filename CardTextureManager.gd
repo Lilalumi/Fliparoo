d3224a5ee2fe97333371987787720e356335987f
Lilalumi <agulilacunado@gmail.com>
2024-10-04 12:16:16 -0300

    card sprites
    card sprites

---
+++ CardTextureManager.gd
@@ -0,0 +1,10 @@
+# CardTextureManager.gd
+extends Node
+
+# Diccionario con los paths base de las texturas
+var card_images = {
+	"hearts": "res://assets/art/sprites/cards/hearts/",
+	"diamonds": "res://assets/art/sprites/cards/diamonds/",
+	"clubs": "res://assets/art/sprites/cards/clubs/",
+	"spades": "res://assets/art/sprites/cards/spades/"
+}
